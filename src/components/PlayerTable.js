5afcb66db1d29732ff4b9d140ee8c494aec02cac
Pierce Hicks <phicks21@gmail.com>
2020-05-29 15:50:09 -0400

    init setup

---
+++ src/components/PlayerTable.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function PlayerTable() {
+    return (
+        <div>
+            <table>
+                <thead>
+                    <th>F. Name</th>
+                    <th>L. Name</th>
+                    <th>Position</th>
+                    <th>Goals</th>
+                    <th>Assists</th>
+                </thead>
+                <tbody>
+                    <td>F. Name</td>
+                    <td>L. Name</td>
+                    <td>Position</td>
+                    <td>Goals</td>
+                    <td>Assists</td>
+                    
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default PlayerTable;
