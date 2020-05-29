6771ab4005afb97d9f989aba97fb662b266955cb
Pierce Hicks <phicks21@gmail.com>
2020-05-29 16:04:41 -0400

    defined dummy data to setup table w Hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PlayerTable from './components/PlayerTable';
 import './App.css';
 
 function App() {
+  const playerData = [
+    {name: "Josef Martinez", position:"Forward", goals:"2", assists:"5", id: 1 },
+    {name: "Pity Martinez", position:"Midfield", goals:"2", assists:"5", id: 1 },
+    {name: "Miles Robinson", position:"Defender", goals:"2", assists:"5", id: 1 }
+  ];
+
+  const [players, setPlayers] = useState(playerData)
   return (
     <div className="App">
-      <PlayerTable />
+      <h1>5v5 Roster Maker</h1>
+      <PlayerTable 
+        players={players}
+      />
     </div>
   );
 }
--- src/components/PlayerTable.js
+++ src/components/PlayerTable.js
@@ -1,23 +1,24 @@
 import React from 'react';
 
-function PlayerTable() {
+function PlayerTable(props) {
     return (
         <div>
             <table>
                 <thead>
-                    <th>F. Name</th>
-                    <th>L. Name</th>
+                    <th>Name</th>
                     <th>Position</th>
                     <th>Goals</th>
                     <th>Assists</th>
                 </thead>
                 <tbody>
-                    <td>F. Name</td>
-                    <td>L. Name</td>
+                    <td>Name</td>
                     <td>Position</td>
                     <td>Goals</td>
                     <td>Assists</td>
-                    
+                    <td>
+                        <button>Edit</button>
+                        <button>Delete</button>
+                    </td>
                 </tbody>
             </table>
         </div>
