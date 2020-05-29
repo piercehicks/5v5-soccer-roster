aa5cce1b8abe58fc36b49df81b0b1e8117f940fb
Pierce Hicks <phicks21@gmail.com>
2020-05-29 17:03:49 -0400

    add form init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PlayerTable from './components/PlayerTable';
+import AddPlayer from './components/AddPlayer';
 import './App.css';
 
 function App() {
@@ -9,10 +10,18 @@ function App() {
     {name: "Miles Robinson", position:"Defender", goals:"2", assists:"5", id: 1 }
   ];
 
-  const [players, setPlayers] = useState(playerData)
+  const [players, setPlayers] = useState(playerData);
+
+  const addPlayer = player => {
+    player.id = player.length + 1
+    setPlayers([...players, player])
+  }
   return (
     <div className="App">
       <h1>5v5 Roster Maker</h1>
+      <AddPlayer 
+        addPlayer={addPlayer}
+      />
       <PlayerTable 
         players={players}
       />
